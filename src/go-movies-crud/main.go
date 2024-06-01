4c68e4e1b2bd98a9914803807d99791d2e34ca30
Thar Htoo <thar.htoo@pixelsmagic.com>
2024-06-01 22:28:23 +0700

    2 pj complete

---
+++ src/go-movies-crud/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, v := range movies {
+		if v.ID == params["id"] {
+			movies = append(movies[:i], movies[i+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, v := range movies {
+		if v.ID == params["id"] {
+			json.NewEncoder(w).Encode(v)
+			return
+		}
+	}
+	http.Error(w, "404 not found", http.StatusNotFound)
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for i, v := range movies {
+		if v.ID == params["id"] {
+			movies = append(movies[:i], movies[i+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = strconv.Itoa(rand.Intn(100000000))
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+	http.Error(w, "404 not found", http.StatusNotFound)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "123456",
+		Title: "La La Land",
+		Director: &Director{
+			FirstName: "Damien",
+			LastName:  "Chazelle",
+		},
+	})
+	movies = append(movies, Movie{
+		ID:    "2",
+		Isbn:  "123457",
+		Title: "In The Mood For Love",
+		Director: &Director{
+			FirstName: "Wong",
+			LastName:  "Kar-wai",
+		},
+	})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
---
+++ src/go-server/go.mod
@@ -0,0 +1,3 @@
+module etharrra/go-server
+
+go 1.22.2
